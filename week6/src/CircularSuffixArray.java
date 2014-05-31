3b2c9a989dd194adbe3f3d17423efcbc5e18240c
Marin Martinic <corax34@gmail.com>
2014-05-31 16:50:55 +0200

    week6 add insertion sort cutoff

--- week6/src/CircularSuffixArray.java
+++ week6/src/CircularSuffixArray.java
@@ -1,6 +1,7 @@
 public class CircularSuffixArray {
 
     private static final int R = 256;
+    private static final int CUTOFF = 15;   // cutoff to insertion sort
 
     private int[] offsets;
     private String s;
@@ -19,15 +20,15 @@ public class CircularSuffixArray {
 
     // msd
     private void sort() {
-        final int W = s.length();
         final int N = offsets.length;
         int[] aux = new int[N];
-        sort(0, N, 0, aux, W);
+        sort(0, N, 0, aux);
     }
 
-    private void sort(int start, int end, int d, int[] aux, int W) {
+    private void sort(int start, int end, int d, int[] aux) {
 
-        if (d == W - 1 || start >= end - 1) {
+        if (end - start < CUTOFF) {
+            insertion(start, end, d);
             return;
         }
 
@@ -51,9 +52,36 @@ public class CircularSuffixArray {
         }
 
         for (int i = 0; i < R; i++) {
-            sort(start + count[i], start + count[i + 1], d + 1, aux, W);
+            sort(start + count[i], start + count[i + 1], d + 1, aux);
         }
+    }
 
+    private void insertion(int start, int end, int d) {
+        for (int i = start; i < end; i++) {
+            for (int j = i; j > start && less(j, j - 1, d); j--) {
+                exch(j, j - 1);
+            }
+        }
+    }
+
+    private boolean less(int x1, int x2, int d) {
+        for (int i = d; i < s.length(); i++) {
+            char c1 = chatAt(offsets[x1], i);
+            char c2 = chatAt(offsets[x2], i);
+            if (c1 < c2) {
+                return true;
+            }
+            else if (c1 > c2) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private void exch(int x1, int x2) {
+        int temp = offsets[x1];
+        offsets[x1] = offsets[x2];
+        offsets[x2] = temp;
     }
 
     private char chatAt(int offset, int d) {
@@ -73,7 +101,7 @@ public class CircularSuffixArray {
     public static void main(String[] args) {
         String s = "ABRACADABRA!";
 
-        In in = new In("D:\\myProjects\\coursera\\alg2\\week6\\burrows\\amendments.txt");
+        In in = new In("D:\\myProjects\\coursera\\alg2\\week6\\burrows\\aesop.txt");
 
         Stopwatch stopwatch = new Stopwatch();
         CircularSuffixArray circularSuffixArray = new CircularSuffixArray(in.readAll());
